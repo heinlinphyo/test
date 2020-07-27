72832b37522de6f4fd99900030776b58f3851212
heinlinphyo <heinlinphyo95@gmail.com>
2020-07-27 17:01:20 +0630

    test

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+(function($){
+
+    "use strict";
+
+    // navbar fixed when page scroll //
+    $(window).scroll(function(){
+        var window_top = $(window).scrollTop() + 1;
+        if(window_top > 50){
+            $('.main_menu').addClass('menu_fixed animated fadeInDown');
+        }else{
+            $('.main_menu').removeClass('menu_fixed animated fadeInDown');
+        }
+    });
+
+
+}(jQuery));
+
